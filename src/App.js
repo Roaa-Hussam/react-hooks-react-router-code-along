ed805a8d5ea44d3643198877c670bcb96aadc28b
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-23 23:46:14 +0300

    Done

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./Home"
+import About from "./About"
+import Login from "./Login"
+import NavBar from "./NavBar"
+
+export default function App() {
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
---
+++ src/Login .js
@@ -0,0 +1,22 @@
+export default function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <input type='text' name='username' placeholder="Username" />
+                </div>
+                <div>
+                    <input type='password' name='password' placeholder="Password" />
+                </div>
+                <input type="submit" value="submit" />
+            </form>
+        </div>
+    )
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
